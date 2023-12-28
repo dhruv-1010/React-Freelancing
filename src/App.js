60dc25e1c0050a8e9269417e052bc97e82b7a16e
Abhishek Sharma <abhisheksharma32344@gmail.com>
2023-12-28 10:05:53 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/Pages/DashBoard.js
    	new file:   src/clinets/projects/AddMilestone.js
    	new file:   src/clinets/projects/Form.js
    	new file:   src/clinets/projects/Running.js
    	new file:   src/freelancer/Profile.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
-import {Route,Routes} from 'react-router-dom'
+import {Form, Route,Routes} from 'react-router-dom'
 import Welcome from "./Pages/Welcome";
+import DashBoard from "./Pages/DashBoard";
+import Running from "./clinets/projects/Running";
+import Profile from "./freelancer/Profile";
 
 
 function App() {
@@ -10,6 +13,10 @@ function App() {
 <Route path="/" element={<Home />}/>
 <Route path="*" element={<Error />}/>
 <Route path="/welcome" element={<Welcome />}/>
+<Route path="/dashboard" element={<DashBoard />}/>
+<Route path="/projects/running" element={<Running />}/>
+<Route path="/projects/form" element={< Form/>}/>
+<Route path="/profile" element={< Profile/>}/>
 </Routes>
   );
 }
